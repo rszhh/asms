7c8e41d2b8e15efffb76b2f19816884bca652db2
zhaohengheng <zhaohengheng@outlook.com>
2020-09-22 19:38:56 +0800

    optimize js code

--- nginx/html/js/chunk-266b8966.7a5493d9.js
+++ nginx/html/js/chunk-266b8966.7a5493d9.js
@@ -1,1 +1,83 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-266b8966"],{"0f66":function(t,a,e){},"7c78":function(t,a,e){"use strict";e.r(a);var o=function(){var t=this,a=t.$createElement,e=t._self._c||a;return e("d2-container",[e("template",{slot:"header"},[t._v("汽车类型销售统计")]),e("div",{staticClass:"inner"},[e("ve-pie",t._b({attrs:{data:t.chartData}},"ve-pie",t.pubSetting,!1))],1)],2)},n=[],s=e("cebe"),r=e.n(s),i=e("7923"),c={mixins:[i["default"]],data:function(){return{chartData:{columns:["type","froportion"],rows:[{type:"家用轿车",froportion:0},{type:"SUV",froportion:0},{type:"越野车",froportion:0}]}}},mounted:function(){this.getData()},methods:{getData:function(){var t=this,a="http://39.98.108.48:5000/type";r.a.post(a).then((function(a){200===a.data.code?0===a.data.length?t.$message({showClose:!0,message:"暂无信息",type:"info"}):(t.chartData.rows[0].froportion=a.data.saloon,t.chartData.rows[1].froportion=a.data.suv,t.chartData.rows[2].froportion=a.data.cross):t.$message({showClose:!0,message:a.data.message,type:"error"}),t.loading=!1})).catch((function(a){t.$message({showClose:!0,message:a,type:"error"})}))}}},p=c,f=(e("85f5"),e("2877")),u=Object(f["a"])(p,o,n,!1,null,"388e05d6",null);a["default"]=u.exports},"85f5":function(t,a,e){"use strict";var o=e("0f66"),n=e.n(o);n.a}}]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([
+    ["chunk-266b8966"], {
+        "0f66": function (t, a, e) {},
+        "7c78": function (t, a, e) {
+            "use strict";
+            e.r(a);
+            var o = function () {
+                    var t = this,
+                        a = t.$createElement,
+                        e = t._self._c || a;
+                    return e("d2-container", [e("template", {
+                        slot: "header"
+                    }, [t._v("汽车类型销售统计")]), e("div", {
+                        staticClass: "inner"
+                    }, [e("ve-pie", t._b({
+                        attrs: {
+                            data: t.chartData
+                        }
+                    }, "ve-pie", t.pubSetting, !1))], 1)], 2)
+                },
+                n = [],
+                s = e("cebe"),
+                r = e.n(s),
+                i = e("7923"),
+                c = {
+                    mixins: [i["default"]],
+                    data: function () {
+                        return {
+                            chartData: {
+                                columns: ["type", "froportion"],
+                                rows: [{
+                                    type: "家用轿车",
+                                    froportion: 0
+                                }, {
+                                    type: "SUV",
+                                    froportion: 0
+                                }, {
+                                    type: "越野车",
+                                    froportion: 0
+                                }]
+                            }
+                        }
+                    },
+                    mounted: function () {
+                        this.getData()
+                    },
+                    methods: {
+                        getData: function () {
+                            var t = this,
+                                a = "http://localhost:5000/type";
+                            r.a.post(a).then((function (a) {
+                                200 === a.data.code ? 0 === a.data.length ? t.$message({
+                                    showClose: !0,
+                                    message: "暂无信息",
+                                    type: "info"
+                                }) : (t.chartData.rows[0].froportion = a.data.saloon, t.chartData.rows[1].froportion = a.data.suv, t.chartData.rows[2].froportion = a.data.cross) : t.$message({
+                                    showClose: !0,
+                                    message: a.data.message,
+                                    type: "error"
+                                }), t.loading = !1
+                            })).catch((function (a) {
+                                t.$message({
+                                    showClose: !0,
+                                    message: a,
+                                    type: "error"
+                                })
+                            }))
+                        }
+                    }
+                },
+                p = c,
+                f = (e("85f5"), e("2877")),
+                u = Object(f["a"])(p, o, n, !1, null, "388e05d6", null);
+            a["default"] = u.exports
+        },
+        "85f5": function (t, a, e) {
+            "use strict";
+            var o = e("0f66"),
+                n = e.n(o);
+            n.a
+        }
+    }
+]);
--- nginx/html/js/chunk-2d0e95c4.6bd341cc.js
+++ nginx/html/js/chunk-2d0e95c4.6bd341cc.js
@@ -1,1 +1,151 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e95c4"],{"8ccf":function(e,t,a){"use strict";a.r(t);var l=function(){var e=this,t=e.$createElement,a=e._self._c||t;return a("div",[a("el-table",{directives:[{name:"loading",rawName:"v-loading",value:e.loading,expression:"loading"}],staticStyle:{width:"100%"},attrs:{data:e.saleInfo,"max-height":"400"}},[a("el-table-column",{attrs:{fixed:"",prop:"totalnumber",label:"已售出总数量",width:"120"}})],1),a("el-table",{directives:[{name:"loading",rawName:"v-loading",value:e.loading,expression:"loading"}],staticStyle:{width:"100%"},attrs:{data:e.tableData,"max-height":"400"}},[a("el-table-column",{attrs:{fixed:"",prop:"carnumber",label:"汽车编号",width:"120"}}),a("el-table-column",{attrs:{prop:"type",label:"类型",width:"120"}}),a("el-table-column",{attrs:{prop:"brand",label:"品牌",width:"120"}}),a("el-table-column",{attrs:{prop:"color",label:"颜色",width:"120"}}),a("el-table-column",{attrs:{prop:"price",label:"价格",width:"120"}}),a("el-table-column",{attrs:{prop:"entrydate",label:"出厂日期",width:"150"}}),a("el-table-column",{attrs:{prop:"saledate",label:"售出日期",width:"150"}})],1)],1)},n=[],o=a("cebe"),s=a.n(o),i={data:function(){return{tableData:[],loading:!0,saleInfo:[]}},mounted:function(){this.getData()},methods:{getData:function(){var e=this,t="http://39.98.108.48:5000/getinfo";s.a.post(t,JSON.stringify({gettype:1}),{headers:{"Content-Type":"application/x-www-form-urlencoded",Accept:"application/json"}}).then((function(t){if(200===t.data.code)if(0===t.data.length)e.$message({showClose:!0,message:"暂无信息",type:"info"});else{e.tableData=t.data.items;var a={totalnumber:t.data.items.length};e.saleInfo.push(a)}else e.$message({showClose:!0,message:t.data.message,type:"error"});e.loading=!1})).catch((function(t){e.$message({showClose:!0,message:t,type:"error"})}))}}},r=i,d=a("2877"),c=Object(d["a"])(r,l,n,!1,null,null,null);t["default"]=c.exports}}]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([
+    ["chunk-2d0e95c4"], {
+        "8ccf": function (e, t, a) {
+            "use strict";
+            a.r(t);
+            var l = function () {
+                    var e = this,
+                        t = e.$createElement,
+                        a = e._self._c || t;
+                    return a("div", [a("el-table", {
+                        directives: [{
+                            name: "loading",
+                            rawName: "v-loading",
+                            value: e.loading,
+                            expression: "loading"
+                        }],
+                        staticStyle: {
+                            width: "100%"
+                        },
+                        attrs: {
+                            data: e.saleInfo,
+                            "max-height": "400"
+                        }
+                    }, [a("el-table-column", {
+                        attrs: {
+                            fixed: "",
+                            prop: "totalnumber",
+                            label: "已售出总数量",
+                            width: "120"
+                        }
+                    })], 1), a("el-table", {
+                        directives: [{
+                            name: "loading",
+                            rawName: "v-loading",
+                            value: e.loading,
+                            expression: "loading"
+                        }],
+                        staticStyle: {
+                            width: "100%"
+                        },
+                        attrs: {
+                            data: e.tableData,
+                            "max-height": "400"
+                        }
+                    }, [a("el-table-column", {
+                        attrs: {
+                            fixed: "",
+                            prop: "carnumber",
+                            label: "汽车编号",
+                            width: "120"
+                        }
+                    }), a("el-table-column", {
+                        attrs: {
+                            prop: "type",
+                            label: "类型",
+                            width: "120"
+                        }
+                    }), a("el-table-column", {
+                        attrs: {
+                            prop: "brand",
+                            label: "品牌",
+                            width: "120"
+                        }
+                    }), a("el-table-column", {
+                        attrs: {
+                            prop: "color",
+                            label: "颜色",
+                            width: "120"
+                        }
+                    }), a("el-table-column", {
+                        attrs: {
+                            prop: "price",
+                            label: "价格",
+                            width: "120"
+                        }
+                    }), a("el-table-column", {
+                        attrs: {
+                            prop: "entrydate",
+                            label: "出厂日期",
+                            width: "150"
+                        }
+                    }), a("el-table-column", {
+                        attrs: {
+                            prop: "saledate",
+                            label: "售出日期",
+                            width: "150"
+                        }
+                    })], 1)], 1)
+                },
+                n = [],
+                o = a("cebe"),
+                s = a.n(o),
+                i = {
+                    data: function () {
+                        return {
+                            tableData: [],
+                            loading: !0,
+                            saleInfo: []
+                        }
+                    },
+                    mounted: function () {
+                        this.getData()
+                    },
+                    methods: {
+                        getData: function () {
+                            var e = this,
+                                t = "http://localhost:5000/getinfo";
+                            s.a.post(t, JSON.stringify({
+                                gettype: 1
+                            }), {
+                                headers: {
+                                    "Content-Type": "application/x-www-form-urlencoded",
+                                    Accept: "application/json"
+                                }
+                            }).then((function (t) {
+                                if (200 === t.data.code)
+                                    if (0 === t.data.length) e.$message({
+                                        showClose: !0,
+                                        message: "暂无信息",
+                                        type: "info"
+                                    });
+                                    else {
+                                        e.tableData = t.data.items;
+                                        var a = {
+                                            totalnumber: t.data.items.length
+                                        };
+                                        e.saleInfo.push(a)
+                                    }
+                                else e.$message({
+                                    showClose: !0,
+                                    message: t.data.message,
+                                    type: "error"
+                                });
+                                e.loading = !1
+                            })).catch((function (t) {
+                                e.$message({
+                                    showClose: !0,
+                                    message: t,
+                                    type: "error"
+                                })
+                            }))
+                        }
+                    }
+                },
+                r = i,
+                d = a("2877"),
+                c = Object(d["a"])(r, l, n, !1, null, null, null);
+            t["default"] = c.exports
+        }
+    }
+]);
